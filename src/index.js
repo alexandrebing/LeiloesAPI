f9b0ec21962553fda2499f02e78315dfe0f26b8a
Giuseppe Menti <mentifg@gmail.com>
2019-06-21 19:58:29 -0300

    Add cors and helmet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const env = require('./env');
 const logger = require('./logger');
@@ -7,7 +9,8 @@ const authRouter = require('./routers/authRouter');
 const errHandler = require('./middlewares/errHandler');
 
 const app = express();
-
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json({
   limit: '100kb',
 }));
